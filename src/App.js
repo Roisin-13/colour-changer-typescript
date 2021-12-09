cd6d67159a984cc29d9d987938504a4503670c04
Roisin-13 <86969519+Roisin-13@users.noreply.github.com>
2021-12-09 17:56:36 +0000

    colour changer react, state used

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: "#ffffff" };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  //---------
+  componentDidMount() {
+    this.applyColour();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColour();
+  }
+
+
+  //--------
+  randNum() {
+    let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+    return hexArray[Math.floor(Math.random() * hexArray.length)];
+  };
+
+  getColour() {
+    let hex = "#" + this.randNum() + this.randNum() + this.randNum() + this.randNum() + this.randNum() + this.randNum();
+    console.log(hex);
+    console.log(typeof hex);
+    return hex;
+
+  };
+
+  //----------------------------
+
+
+  applyColour() {
+    const colour = this.getColour(this.state.color);
+    document.body.style.background = colour;
+  }
+
+
+  handleClick() {
+    this.setState({ color: this.getColour() });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+
+        <div className="colourBox">
+          <h1>colour is:</h1>
+          <h2>{this.hex}</h2>
+          <button onClick={this.handleClick}>Click Me</button>
+        </div>
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
