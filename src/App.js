792be8e1d6232bfc70c9f6ba145fc1d9872bb661
Roisin-13 <86969519+Roisin-13@users.noreply.github.com>
2021-12-10 18:37:51 +0000

    class changed to function components, so I can use hook

--- src/App.js
+++ src/App.js
@@ -1,66 +1,109 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { color: "#ffffff" };
-    this.handleClick = this.handleClick.bind(this);
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { color: "#ffffff" };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-  //---------
-  componentDidMount() {
-    this.applyColour();
-  }
+//   //---------
+//   componentDidMount() {
+//     this.applyColour();
+//   }
 
-  componentDidUpdate(prevProps, prevState) {
-    this.applyColour();
-  }
+//   componentDidUpdate(prevProps, prevState) {
+//     this.applyColour();
+//   }
 
+
+//   //--------
+//   randNum() {
+//     let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+//     return hexArray[Math.floor(Math.random() * hexArray.length)];
+//   };
+
+//   getColour() {
+//     let hex = "#" + this.randNum() + this.randNum() + this.randNum() + this.randNum() + this.randNum() + this.randNum();
+//     console.log(hex);
+//     console.log(typeof hex);
+//     return hex;
+
+//   };
+
+//   //----------------------------
+
+
+//   applyColour() {
+//     const colour = this.getColour(this.state.color);
+//     document.body.style.background = colour;
+//   }
+
+
+//   handleClick() {
+//     this.setState({ color: this.getColour() });
+//   }
+
+
+//   render() {
+//     return (
+//       <div className="App">
+
+//         <div className="colourBox">
+//           <h1>colour is:</h1>
+//           <h2>{this.hex}</h2>
+//           <button onClick={this.handleClick}>Click Me</button>
+//         </div>
+
+//       </div>
+//     );
+//   }
+// }
+
+
+
+
+//---------
+
+
+const App = () => {
+
+  const [colour, setColour] = useState("#ffffff");
+  document.body.style.background = colour;
 
   //--------
-  randNum() {
+  const randNum = () => {
     let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
     return hexArray[Math.floor(Math.random() * hexArray.length)];
   };
 
-  getColour() {
-    let hex = "#" + this.randNum() + this.randNum() + this.randNum() + this.randNum() + this.randNum() + this.randNum();
-    console.log(hex);
-    console.log(typeof hex);
+  const getColour = () => {
+    let hex = "#" + randNum() + randNum() + randNum() + randNum() + randNum() + randNum();
     return hex;
 
   };
 
   //----------------------------
 
-
-  applyColour() {
-    const colour = this.getColour(this.state.color);
-    document.body.style.background = colour;
+  const handleClick = () => {
+    console.log(getColour());
+    setColour(getColour);
   }
 
-
-  handleClick() {
-    this.setState({ color: this.getColour() });
-  }
-
-
-  render() {
-    return (
-      <div className="App">
-
-        <div className="colourBox">
-          <h1>colour is:</h1>
-          <h2>{this.hex}</h2>
-          <button onClick={this.handleClick}>Click Me</button>
-        </div>
-
+  return (
+    <div className="App">
+      <div className="colourBox">
+        <h1>colour is:</h1>
+        <h2>{getColour()}</h2>
+        <button onClick={handleClick}>Click Me</button>
       </div>
-    );
-  }
-}
+
+    </div>
+  );
+};
 
 
 export default App;
